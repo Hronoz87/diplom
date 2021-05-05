98a8495e49de0f0d73d438608c9187bb81eba6b5
Hronoz87 <hromov_andrey@mail.ru>
2021-05-05 20:23:51 +0300

    Init

---
+++ src/main/java/ru/netology/demo/repository/CloudRepository.java
@@ -0,0 +1,7 @@
+//package ru.netology.demo.repository;
+//
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public class CloudRepository {
+//}
---
+++ src/main/java/ru/netology/demo/service/CloudService.java
@@ -0,0 +1,7 @@
+//package ru.netology.demo.service;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CloudService {
+//}
