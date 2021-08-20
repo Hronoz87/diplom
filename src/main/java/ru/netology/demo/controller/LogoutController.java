be33c0cc8050de9dca1deaec1fbada245e523c50
Gromak Maxim <maximgromak@gmail.com>
2021-08-20 14:43:37 +1000

    updated controllers: field injection is not recomended

--- src/main/java/ru/netology/demo/controller/LogoutController.java
+++ src/main/java/ru/netology/demo/controller/LogoutController.java
@@ -3,7 +3,6 @@ package ru.netology.demo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +15,13 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class LogoutController {
 
-    @Autowired
-    UserService userService;
+    private UserService userService;
+    private JwtTokenUtil jwtTokenUtil;
 
-    @Autowired
-    JwtTokenUtil jwtTokenUtil;
+    public LogoutController(UserService userService, JwtTokenUtil jwtTokenUtil) {
+        this.userService = userService;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
 
     private static final Logger log = LoggerFactory.getLogger(LogoutController.class);
 
